0aa135c670063e06b7cdbc0edbf5c35f7f213ae4
exclusiveabhi <imexclusiveabhi@gmail.com>
2024-07-07 17:39:51 +0530

    07/07/2024

---
+++ server/index.js
@@ -0,0 +1,25 @@
+// server/index.js
+
+const express = require('express');
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/userRoutes'); // Assuming user routes are defined in a separate file
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json()); // Parse JSON request bodies
+
+// Connect to MongoDB
+const mongoURI = 'mongodb+srv://exclusiveabhi:maCdjaRpoWvGczS5@cluster1.baefyre.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+// Routes
+app.use('/api/user', userRoutes); // Mount user routes
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
